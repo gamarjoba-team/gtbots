41ed9379476c27ebef173a8312ae8c1045d011a0
smee <maxim.oransky@gmail.com>
2023-04-11 15:00:13 +0400

    groot langs

--- builtins/grootbot.js
+++ builtins/grootbot.js
@@ -1,16 +1,30 @@
 var responses = [ // taken from https://d23.com/how-to-say-i-am-groot-in-different-languages/
-    "I am Groot.",
-    "Jeg er Groot.",
-    "Ich bin Groot.",
-    "Je s'appelle Groot.",
-    "O wau ‘o Groot.",
-    "Sono Groot.",
+    "Ben Groot'um",
     "Ego sum Groot",
-    "Jestem Groot",
     "Eu sou Groot",
+    "I am Groot",
+    "Ich bin Groot",
+    "Ik ben Groot",
+    "Io sono Groot",
+    "Jag är Groot",
+    "Je s'appelle Groot",
+    "Je suis Groot",
+    "Jeg er Groot",
+    "Jestem Groot",
+    "Mimi ni Groot",
     "O a‘u o Groot",
+    "O wau ‘o Groot",
+    "Olen Groot",
+    "Sono Groot",
+    "Soy Groot",
     "Yo soy Groot",
-    "Mimi ni Groot"
+    "Én vagyok Groot",
+    "אני גרוט",
+    "أنا جروت",
+    "मैं ग्रूट हूँ",
+    "我是格鲁特",
+    "私はグルートです",
+    "나는 그루트입니다"
 ];
 
 updateProfile({
